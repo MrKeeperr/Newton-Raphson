064ded70e93913e1284a5c2540309d0df5b1a8e4
bitz <bitz@bitz.com>
2026-03-04 14:59:37 -0500

    feat: add middleware main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# El middleware permite la comunicación entre la api y el frontend
+origins = [
+    "http://localhost:5173",  # Vite/Svelte server por default
+    "http://localhost:3000",  
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True, 
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 
 @app.get("/")
@@ -9,4 +26,6 @@ def read_root():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
